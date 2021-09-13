e4f07e901a2183db5848e570ab1cd7948c41faea
SofiaaMas <88851045+SofiaaMas@users.noreply.github.com>
2021-09-13 11:23:14 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,24 @@
 def main():
     #escribe tu código abajo de esta línea
+    peso=float(input('Inserte el peso en kg'))
+altura=float(input('Inserte altura en m'))
+
+if(peso<=0) or (altura<=0):
+    print('Revise sus datos, alguno es erróneo')
+else:
+    indice=peso/(altura**2)
+    if altura<=0:
+        print('Revise sus datos, alguno es erróneo')
+    elif indice<20:
+        print('Tiene peso bajo')
+    elif 20<=indice<25:
+        print('Tiene peso Normal')
+    elif 25<=indice<30:
+        print('Tiene sobrepeso')
+    elif 30<=indice<40:
+        print('Tiene obesidad')
+    elif indice>=40:
+        print('Tiene obesidad mórbida')
     pass
 if __name__=='__main__':
     main()
